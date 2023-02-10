5444f740deba8393622892b3ca39dc41b64fbd02
HavaAlex <99481724+HavaAlex@users.noreply.github.com>
2023-02-10 12:18:00 +0100

    Add files via upload

---
+++ társasprojekt/script.js
@@ -0,0 +1,59 @@
+var jatekTer = document.getElementById("jatekter");
+
+var balPanel = document.createElement("div");
+var kartyaBox = document.createElement("div");
+var pontokBox = document.createElement("div");
+var tabla = document.createElement("div");
+var korokBox = document.createElement("div");
+
+function JatekterBetoltes()
+{
+    balPanel.appendChild(kartyaBox);
+    balPanel.appendChild(pontokBox);
+    jatekTer.appendChild(balPanel);
+    jatekTer.appendChild(tabla);
+    jatekTer.appendChild(korokBox);
+
+    kartyaBox.innerHTML = "kartyaBox";
+    pontokBox.innerHTML = "pontokBox";
+    tabla.innerHTML = "tabla";
+    korokBox.innerHTML = "korokBox";
+}
+function JatekterElrendezes()
+{
+    balPanel.id = "balpanel";
+    kartyaBox.id = "kartyabox";
+    pontokBox.id = "pontokbox";
+    tabla.id = "tabla";
+    korokBox.id = "korokbox";
+}
+var k = 0;
+function TablaGeneralas()
+{
+    for(var i = 0; i < 5; i++)
+    {
+        var sorDiv = document.createElement("div");
+        sorDiv.classList += " sordiv";
+        for(var j = 0; j<6;j++)
+        {
+            var oszlopDiv = document.createElement("div");
+            oszlopDiv.classList += " oszlopdiv";
+            oszlopDiv.id = k;
+            k++;
+            sorDiv.appendChild(oszlopDiv);
+        }
+        tabla.appendChild(sorDiv);
+    }
+}
+
+function tablafeltoltes (db)
+{
+
+}
+function Main()
+{
+    JatekterBetoltes();
+    JatekterElrendezes();
+    TablaGeneralas();
+}
+Main();
